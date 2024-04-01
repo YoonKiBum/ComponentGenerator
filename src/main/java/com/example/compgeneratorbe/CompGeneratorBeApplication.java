4909a0a7d4e834225e2e2c12e3134ac7a0d4b9ac
ykbcode <ykbcode@gmail.com>
2024-04-01 20:22:45 +0900

    feature: initialize spring boot project

---
+++ src/main/java/com/example/compgeneratorbe/CompGeneratorBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.compgeneratorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompGeneratorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompGeneratorBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/compgeneratorbe/CompGeneratorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compgeneratorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompGeneratorBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
