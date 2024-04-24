75c8c71665bf86ba15a3178bd41c13984b088dc8
ykbcode <ykbcode@gmail.com>
2024-04-24 21:15:30 +0900

    feature: make oauth

---
+++ src/main/java/com/example/compgeneratorbe/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.compgeneratorbe.repository;
+
+import com.example.compgeneratorbe.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+    void deleteByUserId(Long userId);
+}
