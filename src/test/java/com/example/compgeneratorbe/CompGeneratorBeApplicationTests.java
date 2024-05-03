9b30cc6294f9bbe7c516b647b5a00395cd98a0e0
kwonbokeun <royallouis4069jkl@gmail.com>
2024-05-03 23:08:39 +0900

    delete test

--- src/test/java/com/example/compgeneratorbe/CompGeneratorBeApplicationTests.java
+++ src/test/java/com/example/compgeneratorbe/CompGeneratorBeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.compgeneratorbe;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CompGeneratorBeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.compgeneratorbe;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CompGeneratorBeApplicationTests {
+//
+////	@Test
+////	void contextLoads() {
+////	}
+//
+//}
